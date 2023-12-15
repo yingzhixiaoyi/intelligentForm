2e66c98d4f783dc6f4243ab25486bf912fe53a57
Justin <18818989580@163.com>
2023-12-15 09:44:28 +0800

    增加MIT协议

--- packages/core/components/designer/src/modules/header/index.vue
+++ packages/core/components/designer/src/modules/header/index.vue
@@ -2,7 +2,7 @@
   <header class="epic-header relative">
     <div class="epic-header-item flex-1 items-center flex text-12px text-#3d3d3d">
       <slot name="prefix">
-        <a class="text-black decoration-none items-center flex" href="https://github.com/Kchengz/justin-intelligent-form"
+        <a class="text-black decoration-none items-center flex" href="https://github.com/yingzhixiaoyi/intelligentForm"
           target="_blank">
           <img src="../../../../../static/logo.png" class="w-17px h-17px" alt="" srcset="">
           <span class="ml-4">justin-intelligent-form</span>
